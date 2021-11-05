1b43e4b11e2c5564438c05e2b7720198dc111969
vinicius <vinicius45anacleto@gmail.com>
2021-11-05 10:29:42 -0300

    new

--- laboratorio/funçoes/exercicio_10.c
+++ laboratorio/funçoes/exercicio_10.c
@@ -4,30 +4,29 @@
 int circulo(int r);
 int area(int r);
 int main (){
-    int n1=0, i=0, ret_area, ret_circ;
+    int i=0;
+    float ret_area, ret_circ, n1;
     while (i==0){
     printf("digite o raio: ");
-    scanf("%d", &n1);
+    scanf("%f", &n1);
     if(n1<0){
         return 0;
     }
     ret_area = area(n1);
-    printf("a area é de %d\n", ret_area);
+    printf("a area é de %.0f\n", ret_area);
     ret_circ = circulo(n1);
-    printf("a circuferenica do circulo é : %d\n", ret_circ);
+    printf("a circuferenica do circulo é : %.0f\n", ret_circ);
     }
     system ("pause");
     return 0;
 }
 int area (int r){
-    int a;
-    float pi=3.14159;
+    float a, pi=3.14159;
     a = pi * (r*r);
     return (a);
 }
 int circulo(int r){
-    int c;
-    float pi=3.14159;
+    float c, pi=3.14159;
     c = 2*pi*r;
     return (c);
 }
--- laboratorio/funçoes/exercicio_11.c
+++ laboratorio/funçoes/exercicio_11.c
@@ -4,14 +4,23 @@
 int area (int r);
 int vol (int r);
 int main (){
-    int n1, ret_area, ret_vol;
+    float n1, ret_area, ret_vol;
     printf("digite o valor do raio: ");
-    scanf("%d", &n1);
+    scanf("%f", &n1);
     ret_area = area (n1);
     ret_vol = vol (n1);
+    printf("a area da superficie é %.0f\n", ret_area);
+    printf("a area o volume é %.0f\n", ret_vol);
     system ("pause");
     return 0;
 }
 int area (int r){
-    int 
+    float a, pi=3.14159;
+    a = 4*pi*(r*r);
+    return (a);
+}
+int vol(int r){
+    float v, pi=3.14159;
+    v = 4/3*pi*(r*r*r);
+    return (v);
 }
