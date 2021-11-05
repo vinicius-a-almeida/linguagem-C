20cba9bb896ae30278d5243618ea08a62472c176
vinicius <vinicius45anacleto@gmail.com>
2021-11-05 09:57:01 -0300

    master

--- laboratorio/funçoes/exercicio_10.c
+++ laboratorio/funçoes/exercicio_10.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int area(r);
+int circulo(int r);
+int area(int r);
 int main (){
-    int n1, i=0, ret_area;
-    while (i=0){
+    int n1=0, i=0, ret_area, ret_circ;
+    while (i==0){
     printf("digite o raio: ");
     scanf("%d", &n1);
     if(n1<0){
         return 0;
     }
     ret_area = area(n1);
-    printf("a area é'de %i");
+    printf("a area é de %d\n", ret_area);
+    ret_circ = circulo(n1);
+    printf("a circuferenica do circulo é : %d\n", ret_circ);
     }
+    system ("pause");
+    return 0;
 }
-int area (r){
+int area (int r){
     int a, pi=3.14159;
     a = pi * (r*r);
     return (a);
+}
+int circulo(int r){
+    int c, pi=3.14159;
+    c = 2*pi*r;
+    return (c);
 }
