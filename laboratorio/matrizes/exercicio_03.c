fc0bb402e5fe356ed310ae1662fc82e5c2c9a4a4
vinicius <vinicius45anacleto@gmail.com>
2022-02-24 22:38:48 -0300

    first commit

--- laboratorio/matrizes/exercicio_03.c
+++ laboratorio/matrizes/exercicio_03.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 
 int main (){
-    int mat[5] [5], i, j;
-    for(i=0; i<=5; i++){
-        
+    int mat[5][5], i, j;
+    for(i=0; i<=4; i++){
+        for(j=0; j<=4; j++){
+            mat[i][j]=0;
+            if(i==j){
+                mat[i][j]=1;
+            }
+        }
     }
+    for(i=0; i<=4; i++){
+        for(j=0; j<=4; j++){
+            printf("%d", mat[i][j]);
+        }
+        printf("\n");
+    }
+    system("pause");
+    return 0;
 }
